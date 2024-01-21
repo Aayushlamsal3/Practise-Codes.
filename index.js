9acea833cb6b2ad36964a0b85950cf0cfe91dc35
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2024-01-21 18:23:29 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom"
+import Apps from "./Apps"
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(<BrowserRouter><Apps/></BrowserRouter>,document.getElementById('root'))
