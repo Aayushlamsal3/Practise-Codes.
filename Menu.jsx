7aa70dc5975c0f7280ec0d28f2e4cbfd1cccd169
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2024-01-21 18:22:00 +0545

    Create Menu.jsx

---
+++ Menu.jsx
@@ -0,0 +1,69 @@
+import momo from './Images/momo.jpg'
+import pizza from "./Images/pizza.jpg"
+import bread from "./Images/bread.jpg"
+import rice from "./Images/rice.jpg"
+import salad from "./Images/salad.jpg"
+import roll from "./Images/roll.jpg"
+import pancake from "./Images/pancake.jpg"
+import hotdog from "./Images/hotdog.jpg"
+import burger from "./Images/burger.jpg"
+
+const Menu=[
+    {
+        id:"Momo",
+        name:1,
+        image:momo,
+        category:"lunch"
+    },
+    {
+        id:"Pizza",
+        name:2,
+        image:pizza,
+        category:"dinner"
+    },
+    {
+        id:"Bread & Coffee",
+        name:3,
+        image:bread,
+        category:"breakfast"
+    },
+    {
+        id:"Rice and curry",
+        name:4,
+        image:rice,
+        category:"dinner"
+    },
+    {
+        id:"Pancake with Honey",
+        name:5,
+        image:pancake,
+        category:"breakfast"
+    },
+    {
+        id:"Katti Roll",
+        name:6,
+        image:roll,
+        category:"dinner"
+    },
+    {
+        id:"Special Burgers",
+        name:7,
+        image:burger,
+        category:"lunch"
+    },
+    {
+        id:"Vegetarian Salad",
+        name:8,
+        image:salad,
+        category:"breakfast"
+    },
+    {
+        id:"Hot-Dog",
+        name:9,
+        image:hotdog,
+        category:"lunch"
+    }
+
+]
+
+export default Menu;
