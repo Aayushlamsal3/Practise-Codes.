c35238e681897a6a585eee995678fa7ad4f2f66c
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2024-01-22 11:27:57 +0545

    Update Cards.jsx

--- Cards.jsx
+++ Cards.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
+import Menu from "./Menu"
 
 const Cards = ({items}) => {
   return (
     <>
-       <div className="box">
-    {items.map((elem,ind)=>{
-      const{id,name,image,category}=elem
-      return  (   <div className="card" style={{width: "18rem"}} key={ind}>
-      <img className="card-img-top" src={elem.image} alt="Card image cap"/>
-      <div className="card-body">
-        <h5 className="card-title">{elem.id}</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary">Order Now</a>
-      </div>
-    </div>)
-    })}
-    </div>
+<div className="box">
+{items.map((Value,index)=>{
+  return     <div class="card" style={{width: "18rem"}}>
+  <img class="card-img-top" src={Value.image} alt="Card image cap"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+})}
+</div>
     </>
   )
 }
